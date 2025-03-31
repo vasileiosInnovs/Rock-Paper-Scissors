86966292de945ca3cc49e8d13e3ca3f571e916ce
hagee <hagee.ktr@outlook.com>
2025-03-31 13:24:52 +0300

    Generate code that computes a single round of the game

--- index.js
+++ index.js
@@ -38,3 +38,33 @@ function getHumanChoice () {
 //players score variables
 const humanScore = 0;
 const computerScore = 0;
+
+//single round
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    }
+    if ((humanChoice === 'rock') && (computerChoice === 'scissors')) {
+        console.log('You win! Rock beats scissors.')
+        humanScore ++;
+    } else if ((humanChoice === 'rock') && (computerChoice === 'paper')) {
+        console.log('You lose! Paper beats rock.')
+        computerScore ++;
+    }
+
+    if ((humanChoice === 'paper') && (computerChoice === 'rock')) {
+        console.log('You win! Paper beats rock.')
+        humanScore ++;
+    } else if ((humanChoice === 'paper') && (computerChoice === 'scissors')) {
+        console.log('You lose! Scissors beats paper.')
+        computerScore ++;
+    }
+
+    if ((humanChoice === 'scissors') && (computerChoice === 'paper')) {
+        console.log('You win! Scissors beats paper.')
+        humanScore ++;
+    } else if ((humanChoice === 'scissors') && (computerChoice === 'rock')) {
+        console.log('You lose! Rock beats scissors.')
+        computerScore ++;
+    }
+}
